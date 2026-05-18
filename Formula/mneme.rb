59f1945c1181f13ed137b195e5323d6d65d5e02b
tr0mb1r <val.serkin@gmail.com>
2026-05-18 19:02:03 +0200

    chore(release): bump to v1.0.0 (#5)
    Co-authored-by: Valeriy Serkin <valeriy.serkin@malcrove.com>

--- Formula/mneme.rb
+++ Formula/mneme.rb
@@ -5,23 +5,23 @@ class Mneme < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/tr0mb1r/mneme/releases/download/v0.2.6/mneme-0.2.6-aarch64-apple-darwin.tar.gz"
-      sha256 "69145ec85a7821f86754fc10f4c23ca0b8b01e0cc17507f04732e2024c2d4198"
+      url "https://github.com/tr0mb1r/mneme/releases/download/v1.0.0/mneme-1.0.0-aarch64-apple-darwin.tar.gz"
+      sha256 "4055d90a2f314b93d06405842969de622aa668a4b0486f0b16fcec84b0aaeb1a"
     end
     on_intel do
-      url "https://github.com/tr0mb1r/mneme/releases/download/v0.2.6/mneme-0.2.6-x86_64-apple-darwin.tar.gz"
-      sha256 "bf4d8c2732ee0dc463dfba2ca80e701ac25f27633adc9e43824d08542844d78f"
+      url "https://github.com/tr0mb1r/mneme/releases/download/v1.0.0/mneme-1.0.0-x86_64-apple-darwin.tar.gz"
+      sha256 "1ec52ad90e5cf2842286ee910c27c104f206a1c6c64e401a72f4e46dead3c865"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tr0mb1r/mneme/releases/download/v0.2.6/mneme-0.2.6-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "3d000350d2e77f3d23e41127bc3312b6d2bb1082b0b526727c26ae3667a01c8a"
+      url "https://github.com/tr0mb1r/mneme/releases/download/v1.0.0/mneme-1.0.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "f4b2193a16bac1e8c45db3a736112a0bd68f4ba02e8cba61644ed0259012801c"
     end
     on_intel do
-      url "https://github.com/tr0mb1r/mneme/releases/download/v0.2.6/mneme-0.2.6-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "c83d4f84952e41a9e863c8ba08cfa24f3cfe5f04006c3045d20b13ad1adb2430"
+      url "https://github.com/tr0mb1r/mneme/releases/download/v1.0.0/mneme-1.0.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "f2c0d2fb3503d13e59d835cd42ae7545084000144a3474fbf9b0a000c8c8446e"
     end
   end
 
